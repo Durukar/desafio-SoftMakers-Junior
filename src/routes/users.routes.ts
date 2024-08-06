ed2ca321fc0e80a245361425d49503d405e16fae
Durukar <nightdavila@gmail.com>
2024-08-05 23:19:15 -0300

    Rota de usuarios para login e criação

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,35 +1,32 @@
 import { FastifyInstance } from 'fastify'
-import { prismaClient } from '../lib/prisma-client'
-
-interface Users {
-  name: string
-  email: string
-  password: string
-}
+import { UserLogin, Users } from '../interfaces/user.interface'
+import { UserUseCase } from '../usecases/user.usecase'
 
 async function userRoutes(fastify: FastifyInstance) {
+  const userUseCase = new UserUseCase()
+
   /*
    * POST api/user/create
    */
 
   fastify.post<{ Body: Users }>('/create', async (req, reply) => {
-    const { name, email, password } = req.body
+    const data = req.body
 
-    const user = await prismaClient.users.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-      select: {
-        name: true,
-        email: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    })
+    const user = userUseCase.createUser(data)
 
-    reply.send(user)
+    return reply.send(user)
+  })
+
+  /*
+   * POST api/user/login
+   */
+
+  fastify.post<{ Body: UserLogin }>('/login', async (req, reply) => {
+    const data = req.body
+
+    const userLogin = await userUseCase.login(data)
+
+    return reply.send(userLogin)
   })
 }
 
